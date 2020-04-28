3ca24c20708a71aa4770a93f89002f000c7e6a82
gabrielgio <gabriel.giovanini@pm.me>
2020-04-28 19:01:01 +0200

    Get ConnectionString from environment variable

--- HCrawler.DB/Program.cs
+++ HCrawler.DB/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HCrawler.DB
@@ -25,9 +27,18 @@ namespace HCrawler.DB
         {
             return new ServiceCollection()
                 .AddFluentMigratorCore()
+                .AddScoped<IConfiguration>(x =>
+                {
+                    var env = new EnvironmentVariablesConfigurationProvider();
+                    return new ConfigurationRoot(new []{env});
+                })
                 .ConfigureRunner(rb => rb
                     .AddPostgres()
-                    .WithGlobalConnectionString("Host=localhost;Database=image;Username=postgres;Password=masterkey")
+                    .WithGlobalConnectionString(z =>
+                    {
+                        var configuration = z.GetService<IConfiguration>();
+                        return configuration["ConnectionString"];
+                    })
                     .ScanIn(typeof(Program).Assembly).For.Migrations())
                 .AddLogging(lb => lb.AddFluentMigratorConsole())
                 .BuildServiceProvider(false);
