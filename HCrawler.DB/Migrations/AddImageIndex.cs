bf7c453e8f7a312335932075f41f26066f5a1141
gabrielgio <gabriel.giovanini@pm.me>
2020-05-08 00:27:44 +0200

    Add index to createdOn
    Now the query from `GetAllAsync` is not destroying my server.

---
+++ HCrawler.DB/Migrations/AddImageIndex.cs
@@ -0,0 +1,23 @@
+using FluentMigrator;
+
+namespace HCrawler.DB.Migrations
+{
+
+    [Migration(202005080008, "add image CreatedOn index")]
+    public class AddImageIndex : Migration
+
+    {
+        public override void Up()
+        {
+            Create.Index("images_createdon_index")
+                .OnTable("Images")
+                .OnColumn("CreatedOn")
+                .Descending();
+        }
+
+        public override void Down()
+        {
+            Delete.Index("images_createdon_index");
+        }
+    }
+}
